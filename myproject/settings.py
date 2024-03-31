f7e5b9ac96218e801181f9d6659a92c59d0f0821
Lalit Rawool <lrrawool2503@gmail.com>
2024-03-31 15:04:00 +0530

    Revert "Revert "Merge branch 'main' of https://github.com/SAdulkar/TY_Pro""
    This reverts commit 37b8b7f7514875fe0d45cc212e8e055e8db79f4a.

--- myproject/settings.py
+++ myproject/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'employee',
-    'hr'
+    'hr',
+    'fontawesomefree'
 ]
 
 MIDDLEWARE = [
@@ -140,6 +141,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
